7b4664ab716047e4a27c77a2c0c3f84c71be3367
Kami <zjunwang0930@gmail.com>
2018-12-04 15:46:50 +0800

    no message

--- views/js/main.js
+++ views/js/main.js
@@ -503,9 +503,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.getElementsByClassName('mover');
-  var scroll = (document.body.scrollTop / 1250);
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {    
-    var phase = Math.sin((scrollTop.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
